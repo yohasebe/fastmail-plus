12f2dbe42f4b5bf840a2acd7a3344f8f75b292cc
Stanislav (Stas) Katkov <sk@skylup.com>
2022-09-09 21:16:40 +0700

    improve compitability with firefox

--- background-script.js
+++ background-script.js
@@ -1,30 +1,30 @@
 let badgeTimer;
 const asyncFunctionWithAwait = (message, sender, sendResponse) => {
-  const badgeText = chrome.action.getBadgeText({});
+  const badgeText = firefox.browserAction.getBadgeText({});
   badgeText.then(current => {
     if(badgeTimer) clearInterval(badgeTimer);
     if(message.type === "string"){
       if(current !== ""){
-        chrome.browserAction.setBadgeText({text: "" });
+        firefox.browserAction.setBadgeText({text: "" });
       }
     } else if(message.value == 0){
       if(current !== ""){
-        chrome.browserAction.setBadgeText({text: "" });
+        firefox.browserAction.setBadgeText({text: "" });
       }
     } else if(message.value > 0) {
       const messageValue = String(message.value);
       if(current !== messageValue){
         // chrome.action.setBadgeBackgroundColor({color: "#e84545"});
-        chrome.browserAction.setBadgeText({text: messageValue });
+        firefox.browserAction.setBadgeText({text: messageValue });
       }
     }
     badgeTimer = setInterval(() => {
-      chrome.browserAction.setBadgeText({text: "" });
+      firefox.browserAction.setBadgeText({text: "" });
     },6000);
   });
 }
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+firefox.runtime.onMessage.addListener((message, sender, sendResponse) => {
   asyncFunctionWithAwait(message, sender, sendResponse)
   return true;
 });
--- content/main.js
+++ content/main.js
@@ -101,19 +101,18 @@ const runOnChange = (url) => {
 };
 
 const setNumNewMessages = () => {
-  console.log("x");
   let numNewMessages;
   const inbox = $("li.v-MailboxSource.v-MailboxSource--inbox");
   const badge = inbox.find("span.v-MailboxSource-badge").first().text();
   numNewMessages = badge ? parseInt(badge) : 0;
 
-  if (chrome.runtime?.id && regexMail.test(location.href)){
-    chrome.runtime.sendMessage({
+  if (firefox.runtime?.id && regexMail.test(location.href)){
+    firefox.runtime.sendMessage({
       type: "number",
       value: numNewMessages
     });
   } else {
-    chrome.runtime.sendMessage({
+    firefox.runtime.sendMessage({
       type: "string",
       value: ""
     });
