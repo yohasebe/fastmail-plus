2f08d9e0a8a88cabd7d1772232eff1f28bcf9f07
Yoichiro Hasebe <yohasebe@gmail.com>
2022-09-28 07:47:26 +0900

    Add: maximize settings and note panes

--- content/maximize_message_width.js
+++ content/maximize_message_width.js
@@ -1,10 +1,14 @@
 const maximizeMessageWidth = () => {
   let max_width = `
       <style>
-        div.v-MessageCard.app-contentCard, div.v-Compose.app-contentCard {
+        div.v-MessageCard.app-contentCard,
+        div.v-Compose.app-contentCard,
+        div.v-SettingsPane,
+        div.app-contentCard {
           max-width: 100% !important;
         }
       </style>
       `; 
   $('head').append($(max_width));
 }
+
